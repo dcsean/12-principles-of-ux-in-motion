877665ec26bc96b23e6f164b2f2c61a7e363a18e
Ludovic Riffault <ludovic.riffault@gmail.com>
2017-04-07 17:27:47 +0200

    Added new principles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+var element = document.getElementById("valueChange");
+var i = 1;
 
+
+if (i == 20) {
+  setInterval( decrement, 100);
+} else {
+  setInterval( increment, 100);
+}
+
+
+function increment(){
+    i = i % 360 + 1;
+    element.innerHTML = i;
+    return i;
+}
+
+function deincrement(){
+    i = i % 360 + 1;
+    element.innerHTML = i;
+    return i;
+}
